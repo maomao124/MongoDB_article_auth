e042bbe6ea3c1e00787506daedcd1bffe07326b2
mao <1296193245@qq.com>
2022-11-22 14:11:26 +0800

    MongoDB_article_auth

---
+++ src/main/java/mao/mongodb_article_auth/MongoDbArticleAuthApplication.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbArticleAuthApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MongoDbArticleAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/mongodb_article_auth/MongoDbArticleAuthApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbArticleAuthApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
